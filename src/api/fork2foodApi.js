c88873aed7114a796e38dfe0f8a1313216dc4358
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-05-10 14:41:04 -0700

    :hammer: :sparkles: Add getRecipeDetail api call
    Hook each recipe list item to a an event to retrieve recipe details

--- src/api/fork2foodApi.js
+++ src/api/fork2foodApi.js
@@ -1,12 +1,22 @@
 import { handleResponse, handleError } from './apiUtils'
-const url = 'http://localhost:3001/recipes'
+const baseUrl = 'http://localhost:3001/'
 const getRecipes = async () => {
   try {
-    const response = await fetch(url)
+    const response = await fetch(baseUrl + 'recipes')
+    return handleResponse(response)
+  } catch (error) {
+    handleError(error)
+  }
+}
+const getRecipeDetails = async id => {
+  try {
+    const response = await fetch(
+      `${baseUrl}recipe_sources?recipe.recipe_id=${id}`
+    )
     return handleResponse(response)
   } catch (error) {
     handleError(error)
   }
 }
 
-export { getRecipes }
+export { getRecipes, getRecipeDetails }
